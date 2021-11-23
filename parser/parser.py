d39faffe409b473d887c3951eb733d5f0691c336
GGwp-ctrl <for-games.04@yandex.ru>
2021-11-23 20:40:38 +0300

    add requirements.txt

---
+++ parser/parser.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from config import URL
+
+
+def generate_headers():
+    ua = UserAgent()
+    return {'User-Agent': ua.random}
+
+
+def get_html(url):
+    HEADERS = generate_headers()
+    response = requests.get(url, headers=HEADERS)
+    return response.content
+
+
+def get_td(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.findAll('td', class_='topicview')
+    items2 = soup.findAll('td', class_='answer')
+    res = []
+    for i in range(len(items)):
+        res.append(items[i])
+        res.append(items2[i])
+    return res
+
+
+print(get_td(get_html(URL)))
