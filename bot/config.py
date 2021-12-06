fdbc7b29b67a0504d2ad91acaf58faeb02541035
andrew2004Z <zuikovazd@gmail.com>
2021-12-06 17:36:48 +0300

    Update config

--- bot/config.py
+++ bot/config.py
@@ -3,17 +3,19 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = '2104313952:AAFb6dtxWE8d2vFdEi1k2ZYg81xwNCMz_gA'
-button_hi = KeyboardButton('Каталог заданий')
+
 greet_kb1 = ReplyKeyboardMarkup()
-greet_kb2 = ReplyKeyboardMarkup()
-greet_kb2.add(ReplyKeyboardMarkup('Интенсив'))
-greet_kb2.add(ReplyKeyboardMarkup('Задача дня'))
 for i in range(1, 28):
     but = KeyboardButton(str(i))
     greet_kb1.add(but)
 greet_kb1.add(ReplyKeyboardMarkup('В начало'))
+
 greet_kb = ReplyKeyboardMarkup()
-greet_kb.add(button_hi)
+greet_kb.add(KeyboardButton('Каталог заданий'))
 greet_kb.add(KeyboardButton('Тренировка'))
 greet_kb.add(KeyboardButton('Статистика'))
+
+greet_kb2 = ReplyKeyboardMarkup()
+greet_kb2.add(ReplyKeyboardMarkup('Интенсив'))
+greet_kb2.add(ReplyKeyboardMarkup('Задача дня'))
 greet_kb2.add(ReplyKeyboardMarkup('В начало'))
